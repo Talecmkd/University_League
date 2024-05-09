02277905beb60f15daeb842ceaa22eed617250bd
Nik0la23 <nikolanedeski123@gmail.com>
2024-05-10 01:06:56 +0200

    liga

---
+++ Liga/src/test/java/mk/ukim/finki/wp/liga/LigaApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.liga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
